916f74bfef0face9d6662c576ffdd42bbb0591f0
yash-sethia <56500374+yash-sethia@users.noreply.github.com>
2021-04-04 01:28:42 +0530

    Build Folder added

---
+++ clean_fit/build/precache-manifest.93b0d6676874ab1175c7aaa6fbb1e282.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d502fa5bad303fb50689be282ae7fb6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a35c2fdcb7080b5db97b",
+    "url": "/static/css/2.d208ffc4.chunk.css"
+  },
+  {
+    "revision": "63aad2ced9db5f590b37",
+    "url": "/static/css/main.8966103d.chunk.css"
+  },
+  {
+    "revision": "a35c2fdcb7080b5db97b",
+    "url": "/static/js/2.9d809ff3.chunk.js"
+  },
+  {
+    "revision": "63aad2ced9db5f590b37",
+    "url": "/static/js/main.2fcf3ca3.chunk.js"
+  },
+  {
+    "revision": "245afaf503195c25c888",
+    "url": "/static/js/runtime-main.7e14058a.js"
+  },
+  {
+    "revision": "95b56f2300e8d30489509beea89ec3bf",
+    "url": "/static/media/Calender.95b56f23.png"
+  },
+  {
+    "revision": "2d461fe6f8d0e348979f19dc35a386b5",
+    "url": "/static/media/Clean-Fit (1).2d461fe6.png"
+  },
+  {
+    "revision": "0dd685ec90b2a20e5348ad3158ce6044",
+    "url": "/static/media/Clean-Fit (2).0dd685ec.png"
+  },
+  {
+    "revision": "68627cda2c37c79f0204e29bbe189eff",
+    "url": "/static/media/Clean-Fit (3).68627cda.png"
+  },
+  {
+    "revision": "ba02f7e626aa4655caf3a9261408b3e9",
+    "url": "/static/media/Clean-Fit (4).ba02f7e6.png"
+  },
+  {
+    "revision": "f70e0f32ff599e704ac83e1884c5f378",
+    "url": "/static/media/Clean-Fit (5).f70e0f32.png"
+  },
+  {
+    "revision": "d1edf2c29a04271434c89383c01d1aee",
+    "url": "/static/media/Clean-Fit (6)-cutout.d1edf2c2.png"
+  },
+  {
+    "revision": "3bd2e62c510d3f3fe78023d27eab0f70",
+    "url": "/static/media/Clean-Fit.3bd2e62c.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "ac5b572c4417f3d9ae09e68cf3da8803",
+    "url": "/static/media/distance.ac5b572c.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "e4488e923113e7f63ebe16ae0c7437ff",
+    "url": "/static/media/streak.e4488e92.png"
+  }
+]);
